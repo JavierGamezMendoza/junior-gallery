5e413772eb1e6011b5790e77bf625e4192762421
Javier Gámez mendoza <javigm1012@gmail.com>
2024-09-13 14:16:20 +0200

    Project structure

---
+++ src-backend/src/main/java/com/app/juniorgallery/JuniorgalleryApplication.java
@@ -0,0 +1,13 @@
+package com.app.juniorgallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorgalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorgalleryApplication.class, args);
+	}
+
+}
---
+++ src-backend/src/test/java/com/app/juniorgallery/JuniorgalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.juniorgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorgalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
